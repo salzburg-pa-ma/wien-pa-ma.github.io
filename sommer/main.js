79867c853d8a925113b7282f0fe021e8fec85fa2
mariaheinrich97 <102307962+mariaheinrich97@users.noreply.github.com>
2022-06-20 12:15:37 +0200

    RAhmen für alle Inhalte

--- sommer/main.js
+++ sommer/main.js
@@ -19,7 +19,7 @@ let overlay = {
     waldspielplaetze: L.featureGroup(),
     spielplaetze: L.featureGroup(),
     grillzonen: L.featureGroup(),
-    wanderwege: L.featureGroup(),
+    parkanlagen: L.featureGroup(),
     fussgaenger: L.featureGroup(),
 };
 
@@ -36,7 +36,7 @@ let layerControl = L.control.layers({
     "Waldspielplätze": overlay.waldspielplaetze,
     "Spielplätze": overlay.spielplaetze,
     "Grillzonen": overlay.grillzonen,
-    "Wanderwege": overlay.wanderwege,
+    "Parkanlagen": overlay.parkanlagen,
     "Fußgänger": overlay.fussgaenger
 }).addTo(map)
 
@@ -71,54 +71,166 @@ L.control.polylineMeasure().addTo(map);
 
 // FUNKTIONEN AUFRUFEN und GeoJSON einspielen
 
-async function loadBaden(url) {
-        let response = await fetch(url);
-        let geojson = await response.json();
-        //console.log(geojson); //nur ums in der Console zu sehen
+//Fußgängerzonen Vienna Sightseeing -------------------------------------------------------------
+async function loadZones(url) { //anders
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log("Fußgänger", geojson); //nur ums in der Console zu sehen
 
-        L.geoJSON(geojson, {
-            pointToLayer: function (geoJsonPoint, latlng) {
-                let popup = `
+    L.geoJSON(geojson, {
+        style: function (feature) {
+            return {
+                color: "#F012BE",
+                weight: 1,
+                opacity: 0.9,
+                //fillColor: "#F012BE",
+                fillOpacity: 0.5,
+            }
+        }
+    }).bindPopup(function (layer) {
+        return `
+        <h4>Fußgängerzone ${layer.feature.properties.ADRESSE}</h4>
+        <p>Zeitraum: ${layer.feature.properties.ZEITRAUM || ""}</p>
+        <p>${layer.feature.properties.AUSN_TEXT || ""}</p>
+        `;
+    }).addTo(overlay.fussgaenger);
+}
+loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
+
+// Badestellen Vienna -------------------------------------------------------------
+async function loadBaden(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log(geojson); //nur ums in der Console zu sehen
+    L.geoJSON(geojson, {
+        pointToLayer: function (geoJsonPoint, latlng) {
+            let popup = `
                 Name/Standort: <br><strong>${geoJsonPoint.properties.BEZEICHNUNG}</strong>
                 <hr>
                 Wassertemperatur: ${geoJsonPoint.properties.WASSERTEMPERATUR}<br>
                 Wasserqualität: ${geoJsonPoint.properties.BADEQUALITAET}<br>
                 <a href="${geoJsonPoint.properties.WEITERE_INFO}" target="_blank" >Weblink</a>
         `;
-                return L.marker(latlng, {
-                    icon: L.icon({
-                        iconUrl: `../icons/baden.png`,
-                        iconAnchor: [16, 37],
-                        popupAnchor: [0, -37]
-                    })
-                }).bindPopup(popup);
-            }
-        }).addTo(overlay.badestellen);
-    }
-        loadBaden("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BADESTELLENOGD&srsName=EPSG:4326&outputFormat=json");
-
-        //Fußgängerzonen Vieanna Sightseeing
-        async function loadZones(url) { //anders
-            let response = await fetch(url);
-            let geojson = await response.json();
-            console.log("Fußgänger", geojson); //nur ums in der Console zu sehen
-
-            L.geoJSON(geojson, {
-                style: function (feature) {
-                    return {
-                        color: "#F012BE",
-                        weight: 1,
-                        opacity: 0.9,
-                        //fillColor: "#F012BE",
-                        fillOpacity: 0.5,
-                    }
-                }
-            }).bindPopup(function (layer) {
-                return `
-        <h4>Fußgängerzone ${layer.feature.properties.ADRESSE}</h4>
-        <p>Zeitraum: ${layer.feature.properties.ZEITRAUM || ""}</p>
-        <p>${layer.feature.properties.AUSN_TEXT || ""}</p>
-        `;
-            }).addTo(overlay.fussgaenger);
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `../icons/baden.png`,
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            }).bindPopup(popup);
         }
-        loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
+    }).addTo(overlay.badestellen);
+}
+loadBaden("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BADESTELLENOGD&srsName=EPSG:4326&outputFormat=json");
+
+
+
+//Parkanlagen Vienna  -------------------------------------------------------------
+async function loadPark(url) { //anders
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log("Park", geojson); //nur ums in der Console zu sehen
+
+    L.geoJSON(geojson, {
+        pointToLayer: function (geoJsonPoint, latlng) {
+            let popup = `
+                Name/Standort: <br><strong>${geoJsonPoint.properties.BEZEICHNUNG}</strong>
+                <hr>
+                Wassertemperatur: ${geoJsonPoint.properties.WASSERTEMPERATUR}<br>
+                Wasserqualität: ${geoJsonPoint.properties.BADEQUALITAET}<br>
+                <a href="${geoJsonPoint.properties.WEITERE_INFO}" target="_blank" >Weblink</a>
+        `;
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `../icons/urbanpark.png`,
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            }).bindPopup(popup);
+        }
+    }).addTo(overlay.parkanlagen);
+}
+loadPark("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:PARKINFOOGD&srsName=EPSG:4326&outputFormat=json");
+
+
+// Spielplätze -------------------------------------------------------------
+async function loadSpiel(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log(geojson); //nur ums in der Console zu sehen
+    
+
+    L.geoJSON(geojson, {
+        pointToLayer: function (geoJsonPoint, latlng) {
+            let popup = `
+            Name/Standort: <br><strong>${geoJsonPoint.properties.BEZEICHNUNG}</strong>
+            <hr>
+            Wassertemperatur: ${geoJsonPoint.properties.WASSERTEMPERATUR}<br>
+            Wasserqualität: ${geoJsonPoint.properties.BADEQUALITAET}<br>
+            <a href="${geoJsonPoint.properties.WEITERE_INFO}" target="_blank" >Weblink</a>
+    `;
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `../icons/spielplatz.png`,
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            }).bindPopup(popup);
+        }
+    }).addTo(cspielplaetze);
+}
+loadSpiel("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SPIELPLATZPUNKTOGD&srsName=EPSG:4326&outputFormat=json");
+
+// Waldspielplätze -------------------------------------------------------------
+async function loadWaldspiel(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log(geojson); //nur ums in der Console zu sehen
+    L.geoJSON(geojson, {
+        pointToLayer: function (geoJsonPoint, latlng) {
+            let popup = `
+            Name/Standort: <br><strong>${geoJsonPoint.properties.BEZEICHNUNG}</strong>
+            <hr>
+            Wassertemperatur: ${geoJsonPoint.properties.WASSERTEMPERATUR}<br>
+            Wasserqualität: ${geoJsonPoint.properties.BADEQUALITAET}<br>
+            <a href="${geoJsonPoint.properties.WEITERE_INFO}" target="_blank" >Weblink</a>
+    `;
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `../icons/wspielplatz.png`,
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            }).bindPopup(popup);
+        }
+    }).addTo(overlay.waldspielplaetze);
+}
+loadWaldspiel("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WALDSPIELPLOGD&srsName=EPSG:4326&outputFormat=json");
+
+
+// Grillzonen -------------------------------------------------------------
+async function loadGrill(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log(geojson); //nur ums in der Console zu sehen
+    L.geoJSON(geojson, {
+        pointToLayer: function (geoJsonPoint, latlng) {
+            let popup = `
+            Name/Standort: <br><strong>${geoJsonPoint.properties.BEZEICHNUNG}</strong>
+            <hr>
+            Wassertemperatur: ${geoJsonPoint.properties.WASSERTEMPERATUR}<br>
+            Wasserqualität: ${geoJsonPoint.properties.BADEQUALITAET}<br>
+            <a href="${geoJsonPoint.properties.WEITERE_INFO}" target="_blank" >Weblink</a>
+    `;
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `../icons/barbeque.png`,
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            }).bindPopup(popup);
+        }
+    }).addTo(overlay.grillzonen);
+}
+loadGrill("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:GRILLZONEOGD&srsName=EPSG:4326&outputFormat=json");
+
